08e2c404db6f14ced2a360ae1d50db9eb15d913b
lucas-stallknecht <lucas.stallknecht.pro@gmail.com>
2025-05-29 17:02:29 +0200

    Initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,37 @@
+require("config")
+
+-- Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = {
+        -- import your plugins
+        { import = "plugins" },
+    },
+    checker = { enabled = true },
+})
+
+-- Color theme
+vim.cmd [[colorscheme vague]]
+vim.api.nvim_set_hl(0, "QuickFixLine", { fg = "#ffffff", bg = "NONE", bold = true })
+
+-- Transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+
